016ecccc775962ffa831bf3b063510becb21a815
Victor Manuel Aucarure Oliva <olivavictor936@gmail.com>
2026-03-23 14:27:24 -0500

    crud usuarios

--- .history/index_20260323113817.js
+++ .history/index_20260323113817.js
@@ -6,9 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.json({ mensaje: 'API de usuarios funcionando ✅' });
-});
+
 pool.query(`
   CREATE TABLE IF NOT EXISTS usuarios (
     id     SERIAL PRIMARY KEY,
